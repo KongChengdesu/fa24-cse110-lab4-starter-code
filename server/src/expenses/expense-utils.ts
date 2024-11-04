7c788b5b4a692630d954604dfc19aa2cfc68b698
KongChengdesu <77406643+KongChengdesu@users.noreply.github.com>
2024-11-04 13:38:29 -0800

    Exercise done

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -22,7 +22,7 @@ export async function createExpenseServer(req: Request, res: Response, db: Datab
 
 }
 
-export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
+export function deleteExpense(req: Request, res: Response, db: Database) {
     // TO DO: Implement deleteExpense function
     const id = req.params.id;
 
@@ -30,10 +30,28 @@ export function deleteExpense(req: Request, res: Response, expenses: Expense[]) 
         return res.status(400).send({ error: "Missing required fields" });
     }
 
-    expenses.splice(expenses.findIndex(expense => expense.id === id), 1);
-    res.status(204).send();
+    try {
+        // check if the expense exists
+        const expense = db.get('SELECT * FROM expenses WHERE id = ?;', [id]);
+        if (!expense) {
+            return res.status(404).send({ error: "Expense not found" });
+        }
+
+        db.run('DELETE FROM expenses WHERE id = ?;', [id]);
+        res.status(204).send();
+    } catch (error) {
+        return res.status(500).send({ error: `Error deleting expense: ${error}` });
+    }
 }
 
-export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
-    res.status(200).send({ "data": expenses });
+export async function getExpenses(req: Request, res: Response, db: Database) {
+
+    try {
+        const expenses = await db.all('SELECT * FROM expenses');
+        res.status(200).send({ "data": expenses });
+
+    } catch (error) {
+        return res.status(500).send({ error: `Error fetching expenses: ${error}` });
+    }
+
 }
